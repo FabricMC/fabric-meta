35ef5c8d47f6a61bcbd3007493148e1035c07b75
Luke Schwager <luke@blucobalt.dev>
2021-07-10 03:30:07 -0700

    Revert "remove unused methods (fixes crash)"
    This reverts commit 0be01472
    
    Signed-off-by: Luke Schwager <luke@blucobalt.dev>

--- src/main/java/net/fabricmc/meta/web/models/MavenBuildVersion.java
+++ src/main/java/net/fabricmc/meta/web/models/MavenBuildVersion.java
@@ -21,6 +21,7 @@ public class MavenBuildVersion extends MavenVersion {
 
 	String name;
 	String separator;
+	long build;
 
 	public MavenBuildVersion(String maven) {
 		super(maven);
@@ -32,9 +33,19 @@ public class MavenBuildVersion extends MavenVersion {
 		} else {
 			separator = ".";
 		}
+		build = Long.parseLong(version.substring(version.lastIndexOf(".") + 1));
+
 	}
 
 	public String getName() {
 		return name;
+	}
+	
+	public String getSeparator() {
+		return separator;
+	}
+
+	public long getBuild() {
+		return build;
 	}
 }
